8a24577f41a1800dc2a4381eaae10417f5af4cd5
Antonio De la cruz <antoniocruzrivera0216@gmail.com>
2022-02-20 18:40:15 -0600

    commit inicial

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.getElementById("root");
+
+ReactDOM.render(
+  <div>
+    <h2>Hola mundo</h2>
+  </div>,
+  root
+);
+
